73b7eeae6804ae4e0c2f97ecbf8487e7d52caa6f
proguy45 <138774855+proguy45@users.noreply.github.com>
2023-07-11 16:49:07 +0530

    Add files via upload

---
+++ Fibonacci  number finder.py
@@ -0,0 +1,259 @@
+import time
+print ("Hello and welcome to atharv's Fibonacci number finder")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(2)
+a=int(input("enter your number which you want to know is Fibonacci or not: "))
+x=0
+y=1
+z=0
+while(z<=a):
+    x=y
+    y=z
+    z=x+y
+    if(z==a):
+        time.sleep(2)
+        print(a,"is a Fibonacci number") 
+        break
+    elif(z>a):
+        time.sleep(2)
+        print(a,"is not a Fibonacci number")
+time.sleep(10)
